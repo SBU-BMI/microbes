8d05141d6a2d0581486e6fb801a160ab0d17504e
Jonas Almeida <jalmeida@mathbiol.org>
2016-03-31 13:19:17 -0400

    viz test

--- cdiff.js
+++ cdiff.js
@@ -1,1 +1,5 @@
 console.log('loaded cdiff.js');
+
+sbmApps.render("<div class='tableauPlaceholder' style='width: 1384px; height: 1156px;'><object class='tableauViz' width='1384' height='1156' style='display:none;'><param name='host_url' value='https%3A%2F%2Fdiscovery.analytics.healtheintent.com%2F' /> <param name='site_root' value='&#47;t&#47;SBMCIN' /><param name='name' value='CDiff_0&#47;CDiffDiscovery' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='showVizHome' value='n' /><param name='showShareOptions' value='true' /></object></div>")
+//$.getScript('https://discovery.analytics.healtheintent.com/javascripts/api/viz_v1.js')
+$.getScript('https://github.com/sbm-it/microbes/viz_v1.js')
